e590764b971e21ce1fc6672dc49d0bb635c356f9
Samuel Silva <samuelsilvawb@gmail.com>
2024-03-06 23:14:28 +0000

    feat: finish exercise 05

--- src/05/timerMachine.js
+++ src/05/timerMachine.js
@@ -1,9 +1,9 @@
-import { createMachine, assign } from 'xstate';
+import { createMachine, assign } from "xstate";
 
 const timerExpired = (ctx) => ctx.elapsed >= ctx.duration;
 
 export const timerMachine = createMachine({
-  initial: 'idle',
+  initial: "idle",
   context: {
     duration: 5,
     elapsed: 0,
@@ -16,21 +16,21 @@ export const timerMachine = createMachine({
         elapsed: 0,
       }),
       on: {
-        TOGGLE: 'running',
+        TOGGLE: "running",
       },
     },
     running: {
+      always: {
+        target: "expired",
+        cond: timerExpired,
+      },
       on: {
-        // Add an eventless (always) transition that checks if the timer is expired.
-        // If so, go to the `expired` state.
-        // ...
-
         TICK: {
           actions: assign({
             elapsed: (ctx) => ctx.elapsed + ctx.interval,
           }),
         },
-        TOGGLE: 'paused',
+        TOGGLE: "paused",
         ADD_MINUTE: {
           actions: assign({
             duration: (ctx) => ctx.duration + 60,
@@ -40,13 +40,13 @@ export const timerMachine = createMachine({
     },
     paused: {
       on: {
-        TOGGLE: 'running',
-        RESET: 'idle',
+        TOGGLE: "running",
+        RESET: "idle",
       },
     },
     expired: {
       on: {
-        RESET: 'idle',
+        RESET: "idle",
       },
     },
   },
